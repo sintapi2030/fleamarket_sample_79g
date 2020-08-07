35aea8450b5e658612212cd9fc4441e149999993
muchaten <muchaten4567@gmail.com>
2020-08-07 19:20:39 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "items#index"
-  get 'items/sell'
-  get 'item/show'
   resources :users, only: [:show, :edit, :update]
   resources :items do
     collection do #最終的にmemberに切り替える
